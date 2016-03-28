ac65daf0434032a454377b52751925a5b6739c80
diglungdig <tachyon.of.tear@gmail.com>
2016-03-28 01:54:47 -0400

    1

---
+++ Assets/circulingObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class circulingObject : MonoBehaviour {
+
+    public GameObject timeRobot;
+    public GameObject player;
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position;
+        timeRobot.transform.RotateAround(transform.position,Vector3.up, 120 * Time.deltaTime);
+        timeRobot.transform.Rotate(0f, 80f * Time.deltaTime, 0f, Space.Self);
+    }
+}
