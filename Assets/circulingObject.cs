f05d071e8d35063ae7db4a0f9e93efaadb9a2b71
diglungdig <tachyon.of.tear@gmail.com>
2016-04-15 19:07:26 -0400

    audio Visualizer using unity3d

--- Assets/circulingObject.cs
+++ Assets/circulingObject.cs
@@ -5,10 +5,18 @@ public class circulingObject : MonoBehaviour {
 
     public GameObject timeRobot;
     public GameObject player;
+
+	[SerializeField]
+	private bool posLock = false;
 	// Update is called once per frame
 	void Update () {
-        transform.position = player.transform.position;
-        timeRobot.transform.RotateAround(transform.position,Vector3.up, 120 * Time.deltaTime);
+		if (Input.GetKeyDown (KeyCode.E)) {
+			posLock = !posLock;
+		}
+		if (!posLock) {
+			transform.position = player.transform.position;
+		}
+		timeRobot.transform.RotateAround(transform.position,Vector3.up, 120 * Time.deltaTime);
         timeRobot.transform.Rotate(0f, 80f * Time.deltaTime, 0f, Space.Self);
     }
 }
