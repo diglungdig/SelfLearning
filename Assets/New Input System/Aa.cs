a30631c7c805eff25fa371f0aa68c43ba970f6fc
diglungdig <tachyon.of.tear@gmail.com>
2018-05-15 00:41:26 -0400

    New Input System by Unity(doesnt work so far)

---
+++ Assets/New Input System/Aa.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.InputNew;
+
+// GENERATED FILE - DO NOT EDIT MANUALLY
+namespace UnityEngine.InputNew
+{
+	public class Aa : ActionMapInput {
+		public Aa (ActionMap actionMap) : base (actionMap) { }
+		
+		public ButtonInputControl @jump { get { return (ButtonInputControl)this[0]; } }
+	}
+}
---
+++ Assets/New Input System/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputNew;
+
+public class PlayerControl : MonoBehaviour {
+
+    public PlayerInput playerInput;
+
+
+    public ButtonAction jump;
+	// Use this for initialization
+	void Start () {
+        //StartCoroutine(Assign());
+        //newactionmap = playerInput.GetActions<NewActionMap>();
+        jump.Bind(playerInput.handle);
+    }
+
+
+    IEnumerator Assign()
+    {
+        yield return new WaitForSeconds(2f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+            if (jump.control.isHeld)
+            {
+                Debug.Log("Space is pressed: Jump!");
+            }
+        
+	}
+}
